2602a1e2dcba984cd4edc67d162741c5aae52daa
Michael <mkidawa1@gmail.com>
2019-05-31 20:20:23 +0200

    Toggle active panel

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     // 1. Random number
@@ -18,4 +22,17 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     var diceDOM =  document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
 });
